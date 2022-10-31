faecead22480e4b5e52759ae5c32069907369190
Vogy <alexvong14@gmail.com>
2022-10-31 13:22:27 +0000

    View all posts added

--- controllers/posts.js
+++ controllers/posts.js
@@ -12,5 +12,16 @@ module.exports = (app) => {
       // SAVE INSTANCE OF POST MODEL TO DB AND REDIRECT TO THE ROOT
       post.save(() => res.redirect('/'));
     });
+
+    // Render posts
+    app.get('/',  async (req, res) => {
+      try{
+        const posts = await Post.find({}).lean();
+        console.log(posts);
+        return res.render('posts-index', {posts});
+      } catch(err){
+        console.log(err.message);
+      }
+    });
   
   };
